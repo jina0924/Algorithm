3306b0512d00d3acc23e35491aed07ecf38f36c4
jina jeong <jina092400@gmail.com>
2023-04-19 23:51:00 +0900

    Solve BOJ 2015

--- Baekjoon/[BOJ]2015_수들의합4/s2.py
+++ Baekjoon/[BOJ]2015_수들의합4/s2.py
@@ -5,11 +5,14 @@ sys.stdin = open('input.txt')
 from collections import defaultdict
 
 N, K = map(int, input().split())
-arr = [0] + list(map(int, input().split()))
-cnt = {}
+arr = list(map(int, input().split()))
+cnt = defaultdict(int)
 ans = 0
-for i in range(N + 1):
-    arr[i] += arr[i - 1]
-    ans += cnt.get(arr[i] - K, 0)
-    cnt[arr[i]] = cnt.get(arr[i], 0) + 1
+cnt[0] = 1                          # 아무것도 더하지 않은 상태의 누적값 경우의 수 1 올려줌
+total = 0
+for num in arr:
+    total += num                    # 현재 위치까지의 누적합을 담는 변수
+    if cnt[total - K]:              # total - (total - K) = K이므로
+        ans += cnt[total - K]       # 누적합이 (total - K)였던 경우의 수만큼 K를 만들 수 있음
+    cnt[total] += 1                 # 현재 구한 누적합 개수 1 올려줌
 print(ans)
