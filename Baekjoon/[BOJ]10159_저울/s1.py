52bfb15c9722d2a76111e8902d356465517f213f
jina jeong <jina092400@gmail.com>
2023-05-23 16:21:43 +0900

    UPDATE : Add BOJ 10159 comment

--- Baekjoon/[BOJ]10159_저울/s1.py
+++ Baekjoon/[BOJ]10159_저울/s1.py
@@ -14,10 +14,10 @@ for i in range(1, n + 1):
     graph[i][i] = 0
 
 for _ in range(m):
-    a, b = map(int, input().split())
+    a, b = map(int, input().split())            # 무게 비교했을 때 a > b
     graph[a][b] = 1
 
-for k in range(1, n + 1):
+for k in range(1, n + 1):                       # a에서 b로 갈 수 있는 최단 경로 저장
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
@@ -25,6 +25,6 @@ for k in range(1, n + 1):
 for a in range(1, n + 1):
     cnt = 0
     for b in range(1, n + 1):
-        if graph[a][b] == INF and graph[b][a] == INF:
+        if graph[a][b] == INF and graph[b][a] == INF:       # graph[a][b]: a가 b보다 무거운지, graph[b][a]: b가 a보다 무거운지
             cnt += 1
     print(cnt)
