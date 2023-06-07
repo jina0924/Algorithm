e71d0b75d222a39be2a3e497139de831025ca173
jina jeong <jina092400@gmail.com>
2023-06-07 11:09:27 +0900

    Solve BOJ 15971

--- Baekjoon/[BOJ]15971_두로봇/s1.py
+++ Baekjoon/[BOJ]15971_두로봇/s1.py
@@ -1,26 +1,26 @@
-# 백준 15971번 두 로봇 - 46%
+# 백준 15971번 두 로봇 - 41%
 
 import sys
-sys.stdin = open('input.txt')
+sys.stdin = open('input3.txt')
 input = sys.stdin.readline
 from collections import defaultdict
 import heapq
 
 
 def connect():
-    heap = [(0, A, 0), (0, B, 0)]
+    heap = [(0, A, A, 0), (0, B, B, 0)]
     visited = [0] * (N + 1)
-    cnt = [-1] * (N + 1)
-    cnt[A] = cnt[B] = 0
-    visited[A], visited[B] = A, B
+    cnt = [0] * (N + 1)
 
     while heap:
-        cw, cv, cost = heapq.heappop(heap)
-        cnt[cv] = cost
-        visited[cv] = 1
+        cw, cv, s, total = heapq.heappop(heap)
+        cnt[cv] += total
+        visited[cv] = s
         for nv, nw in graph[cv]:
             if not visited[nv]:
-                heapq.heappush(heap, (nw, nv, cnt[cv] + nw))
+                heapq.heappush(heap, (nw, nv, s, total + nw))
+            elif visited[nv] and visited[nv] != s:
+                return cnt[cv] + cnt[nv]
 
 
 N, A, B = map(int, input().split())
