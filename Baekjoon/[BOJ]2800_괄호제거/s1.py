1a8d2b0c2464d09a65fc563989ba6b77be93a8dd
jina0924 <jina092400@gmail.com>
2023-07-16 22:56:19 +0900

    Try to solve BOJ 2800

---
+++ Baekjoon/[BOJ]2800_괄호제거/s1.py
@@ -0,0 +1,33 @@
+# 백준 2800번 괄호 제거
+
+import sys
+sys.stdin = open('input.txt')
+from itertools import combinations as C
+from collections import defaultdict
+
+data = list(input())
+cnt = 0
+brackets = defaultdict(list)
+
+tmp = 1
+for idx in range(len(data)):
+    if data[idx] == '(':
+        brackets[tmp].append(idx)
+        tmp += 1
+    elif data[idx] == ')':
+        tmp -= 1
+        brackets[tmp].append(idx)
+
+ans = []
+cnt = len(brackets)
+for i in range(1, cnt + 1):
+    comb = C(brackets, i)
+    for c in comb:
+        result = data
+        for j in c:
+            s, e = brackets[j]
+            result[s], result[e] = '', ''
+        ans.append(''.join(result))
+
+for a in sorted(ans):
+    print(a)
