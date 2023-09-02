e248028b47d99cffe707c99148b13c590c63c201
jina jeong <jina092400@gmail.com>
2023-09-02 21:32:45 +0900

    Solve PCCP 1_4

--- Programmers/PCCP_1/4_운영체제/s1.py
+++ Programmers/PCCP_1/4_운영체제/s1.py
@@ -2,34 +2,35 @@
 
 import heapq
 
+
 def solution(program):
     answer = [0] * 11
-    program.sort(key = lambda x: (x[1], x[0]))
-    standby = []
-    et = program[0][1]
+    program.sort(key=lambda x: (x[1], x[0]))
+    waiting = []
+    cur = 0
     i = 0
 
-
-    def out(et):
-        j, st, l = heapq.heappop(standby)
-        answer[0] += l
-        answer[j] += et - st
-        et += l
-        return et
-
-
-    while i < len(program):
-        if program[i][1] <= et:
-            heapq.heappush(standby, program[i])
+    def call(i):
+        while i < len(program) and program[i][1] <= cur:
+            heapq.heappush(waiting, program[i])
             i += 1
-        else:
-            et = out(et)
-    while standby:
-        et = out(et)
+        return i
+
+    while i < len(program) or waiting:
+        if not waiting:
+            cur = program[i][1]
+            i = call(i)
+
+        j, st, l = heapq.heappop(waiting)
+        answer[j] += cur - st
+        cur += l
+        i = call(i)
+    answer[0] += cur
+
     return answer
 
 import sys
-sys.stdin = open('input2.txt')
+sys.stdin = open('input1.txt')
 
 N = int(input())
 data = [list(map(int, input().split())) for _ in range(N)]
