2113b3d3558638cdaea670bb126652f339a7a8a5
jina jeong <jina092400@gmail.com>
2023-12-04 21:22:10 +0900

    Solve BOJ 21939

--- Baekjoon/[BOJ]21939_문제추천시스템ver1/s1.py
+++ Baekjoon/[BOJ]21939_문제추천시스템ver1/s1.py
@@ -4,14 +4,25 @@ import sys
 sys.stdin = open('input.txt')
 import heapq
 
+
+def solved_max_out():
+    while not questions[-Mheap[0][1]]:
+        heapq.heappop(Mheap)
+
+
+def solved_min_out():
+    while not questions[mheap[0][1]]:
+        heapq.heappop(mheap)
+
+
 n = int(input())
-heap = []
-heap2 = []
-questions = {}
+mheap = []                              # 난이도 쉬운 순으로 담을 힙
+Mheap = []                              # 난이도 어려운 순으로 담을 힙
+questions = {}                          # 문제 푼 여부 확인용(안 푼 문제=1, 푼 문제=0)
 for _ in range(n):
     p, l = map(int, input().split())
-    heapq.heappush(heap, (l, p))
-    heapq.heappush(heap2, (-l, -p))
+    heapq.heappush(mheap, (l, p))
+    heapq.heappush(Mheap, (-l, -p))
     questions[p] = 1
 m = int(input())
 for _ in range(m):
@@ -19,17 +30,17 @@ for _ in range(m):
     r = data[0]
     if r == 'recommend':
         if data[1] == '1':
-            while not questions[-heap2[0][1]]:
-                heapq.heappop(heap2)
-            print(-heap2[0][1])
+            solved_max_out()
+            print(-Mheap[0][1])
         else:
-            while not questions[heap[0][1]]:
-                heapq.heappop(heap)
-            print(heap[0][1])
+            solved_min_out()
+            print(mheap[0][1])
     elif r == 'add':
         p, l = map(int, data[1:])
-        heapq.heappush(heap, (l, p))
-        heapq.heappush(heap2, (-l, -p))
+        solved_max_out()
+        solved_min_out()
+        heapq.heappush(mheap, (l, p))
+        heapq.heappush(Mheap, (-l, -p))
         questions[p] = 1
     else:
         p = int(data[1])
